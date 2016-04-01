38db2a2507175c1c8e91cec71255efa6bc435a15
Frank Xu <frank.xu@northbright.com>
2016-04-01 22:13:37 +0800

    Return immediately if meet errors in main()

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ func main() {
 	redisSettings, err := server.LoadRedisSettingsFromFile(server.RedisSettingsFile)
 	if err != nil {
 		fmt.Printf("LoadRedisSettingsFromFile(%v) error: %v\n", server.RedisSettingsFile, err)
-	} else {
-		fmt.Printf("LoadRedisSettingsFromFile(%v) OK: %v\n", server.RedisSettingsFile, redisSettings)
+		return
 	}
+	fmt.Printf("LoadRedisSettingsFromFile(%v) OK: %v\n", server.RedisSettingsFile, redisSettings)
 
 	// Create Redis Pools.
 	server.RedisPools, err = server.CreateRedisPools(redisSettings)
 	if err != nil {
 		fmt.Printf("CreateRedisPools(%v) error: %v\n", redisSettings, err)
-	} else {
-		fmt.Printf("CreateRedisPools() OK\n")
+		return
 	}
+	fmt.Printf("CreateRedisPools() OK\n")
 }
